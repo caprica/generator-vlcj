36bce1f465f16e2a3881915e17e7ba771f8a6cfb
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-04-17 00:02:43 +0100

    Fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,27 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
 
-describe('generator-vlcj:app', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+
+describe("generator-vlcj:app", () => {
+  beforeEach(() => {
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+      groupId: "uk.co.caprica",
+      name: "test",
+      description: "test project",
+      packageName: "uk.co.caprica.test",
+      mainClassName: "Main",
+      useComponent: "Y",
+      vlcjVersion: "4.4.1",
+      sourceJdk: "1.8",
+      targetJdk: "1.8",
+      gitIgnore: "Y",
+      gitKeep: "Y",
+    });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["src/main/java/uk/co/caprica/test/Main.java"]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,143 +1,158 @@
-'use strict';
+"use strict";
 
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const mkdirp = require('mkdirp');
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const mkdirp = require("mkdirp");
 
 module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(
+        `Welcome to the super-excellent ${chalk.red(
+          "generator-vlcj"
+        )} generator!`
+      )
+    );
 
-    prompting() {
+    const prompts = [
+      {
+        type: "input",
+        name: "groupId",
+        message: "What is the project Maven groupId?",
+        default: "com.mycompany",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the project name?",
+        default: this.appname.replace(/\s/g, "-"),
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Describe the project:",
+      },
+      {
+        type: "input",
+        name: "packageName",
+        message: "What is the top-level package name for your project?",
+        default: function (answers) {
+          return answers.groupId + "." + answers.name.replace(/-/g, "");
+        },
+      },
+      {
+        type: "input",
+        name: "mainClassName",
+        message: "What is the name for the main class?",
+        default: "Application",
+      },
+      {
+        type: "confirm",
+        name: "useComponent",
+        message: "Use vlcj's component framework?",
+        default: true,
+      },
+      {
+        type: "input",
+        name: "vlcjVersion",
+        message: "Which version of vlcj is required?",
+        default: "4.4.1",
+      },
+      {
+        type: "input",
+        name: "sourceJdk",
+        message: "Which JDK level to use for compiling sources?",
+        default: "1.8",
+      },
+      {
+        type: "input",
+        name: "targetJdk",
+        message: "Which JDK level to use for the compiled class files?",
+        default: function (answers) {
+          return answers.sourceJdk;
+        },
+      },
+      {
+        type: "confirm",
+        name: "gitIgnore",
+        message: "Add .gitignore?",
+        default: true,
+      },
+      {
+        type: "confirm",
+        name: "gitKeep",
+        message: "Add .gitkeep to empty directories?",
+        default: true,
+      },
+    ];
 
-        this.log(
-            yosay(`Welcome to the super-excellent ${chalk.red('generator-vlcj')} generator!`)
-        );
+    return this.prompt(prompts).then((props) => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
 
-        const prompts = [
-            {
-                type: 'input',
-                name: 'groupId',
-                message: 'What is the project Maven groupId?',
-                default: 'com.mycompany'
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the project name?',
-                default: this.appname.replace(/\s/g, '-')
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Describe the project:'
-            },
-            {
-                type: 'input',
-                name: 'packageName',
-                message: 'What is the top-level package name for your project?',
-                default: function(answers) {
-                    return answers.groupId + '.' + answers.name.replace(/\-/g, '');
-                }
-            },
-            {
-                type: 'input',
-                name: 'mainClassName',
-                message: 'What is the name for the main class?',
-                default: 'Application'
-            },
-            {
-                type: 'confirm',
-                name: 'useComponent',
-                message: 'Use vlcj\'s component framework?',
-                default: true
-            },
-            {
-                type: 'input',
-                name: 'vlcjVersion',
-                message: 'Which version of vlcj is required?',
-                default: '4.4.1'
-            },
-            {
-                type: 'input',
-                name: 'sourceJdk',
-                message: 'Which JDK level to use for compiling sources?',
-                default: '1.8'
-            },
-            {
-                type: 'input',
-                name: 'targetJdk',
-                message: 'Which JDK level to use for the compiled class files?',
-                default: function(answers) {
-                    return answers.sourceJdk;
-                }
-            },
-            {
-                type: 'confirm',
-                name: 'gitIgnore',
-                message: 'Add .gitignore?',
-                default: true
-            },
-            {
-                type: 'confirm',
-                name: 'gitKeep',
-                message: 'Add .gitkeep to empty directories?',
-                default: true
-            }
-        ];
+  writing() {
+    mkdirp.sync(this.destinationPath("src/main/java"));
+    mkdirp.sync(this.destinationPath("src/main/resources"));
+    mkdirp.sync(this.destinationPath("src/test/java"));
+    mkdirp.sync(this.destinationPath("src/test/resources"));
 
-        return this.prompt(prompts).then(props => {
-            // To access props later use this.props.someAnswer;
-            this.props = props;
-        });
+    this.fs.copyTpl(
+      this.templatePath(
+        (this.props.useComponent ? "with-component" : "without-component") +
+          "/MainClass.java.tpl"
+      ),
+      this.destinationPath(
+        "src/main/java/" +
+          this.props.packageName.replace(/\./g, "/") +
+          "/" +
+          this.props.mainClassName +
+          ".java"
+      ),
+      {
+        packageName: this.props.packageName,
+        mainClassName: this.props.mainClassName,
+        name: this.props.name,
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("pom.xml"),
+      this.destinationPath("pom.xml"),
+      {
+        name: this.props.name,
+        description: this.props.description,
+        groupId: this.props.groupId,
+        vlcjVersion: this.props.vlcjVersion,
+        sourceJdk: this.props.sourceJdk,
+        targetJdk: this.props.targetJdk,
+      }
+    );
+
+    if (this.props.gitIgnore) {
+      this.fs.copy(
+        this.templatePath("gitignore"),
+        this.destinationPath(".gitignore")
+      );
     }
 
-    writing() {
-        mkdirp.sync(this.destinationPath('src/main/java'));
-        mkdirp.sync(this.destinationPath('src/main/resources'));
-        mkdirp.sync(this.destinationPath('src/test/java'));
-        mkdirp.sync(this.destinationPath('src/test/resources'));
-        this.fs.copyTpl(
-            this.templatePath((this.props.useComponent ? 'with-component' : 'without-component') + '/MainClass.java.tpl'),
-            this.destinationPath('src/main/java/' + this.props.packageName.replace(/\./g, '/') + '/' + this.props.mainClassName + '.java'), {
-                packageName  : this.props.packageName,
-                mainClassName: this.props.mainClassName,
-                name         : this.props.name
-            }
-        );
-        this.fs.copyTpl(
-            this.templatePath('pom.xml'),
-            this.destinationPath('pom.xml'), {
-                name         : this.props.name,
-                description  : this.props.description,
-                groupId      : this.props.groupId,
-                vlcjVersion  : this.props.vlcjVersion,
-                sourceJdk    : this.props.sourceJdk,
-                targetJdk    : this.props.targetJdk
-            }
-        );
-        if (this.props.gitIgnore) {
-            this.fs.copy(
-                this.templatePath('gitignore'),
-                this.destinationPath('.gitignore')
-            );
-        }
-        if (this.props.gitKeep) {
-            this.fs.copy(
-                this.templatePath('gitkeep'),
-                this.destinationPath('src/main/resources/.gitkeep')
-            );
-            this.fs.copy(
-                this.templatePath('gitkeep'),
-                this.destinationPath('src/test/java/.gitkeep')
-            );
-            this.fs.copy(
-                this.templatePath('gitkeep'),
-                this.destinationPath('src/test/resources/.gitkeep')
-            );
-        }
+    if (this.props.gitKeep) {
+      this.fs.copy(
+        this.templatePath("gitkeep"),
+        this.destinationPath("src/main/resources/.gitkeep")
+      );
+      this.fs.copy(
+        this.templatePath("gitkeep"),
+        this.destinationPath("src/test/java/.gitkeep")
+      );
+      this.fs.copy(
+        this.templatePath("gitkeep"),
+        this.destinationPath("src/test/resources/.gitkeep")
+      );
     }
+  }
 
-    install() {
-    }
-
-  };
+  install() {}
+};
